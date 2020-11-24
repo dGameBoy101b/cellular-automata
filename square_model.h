450e00413ef4dbf6ee247bb3b40203f32ec9e744
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2020-11-24 17:51:09 +0800

    Square Model builder
    SquareModel class that builds a Model that is square.

---
+++ square_model.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "model.h"
+
+namespace Data
+{
+	class SquareModel
+	{
+	public:
+		/**
+		 * @brief Build a Model that is square.
+		 * 
+		 * @return A normalised Model that describes a square along the XY plane.
+		 */
+		static const Model getModel();
+	};
+}
