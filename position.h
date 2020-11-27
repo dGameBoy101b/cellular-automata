e730d3c9cc543bb9f79cee7a6fa8b8d5d0ffce19
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2020-11-27 14:42:40 +0800

    Linking and Templating Fixes
    Fixed the linker settings to link the Freeglut debug library.
    Moved the Position class template implementation to the header file.

--- position.h
+++ position.h
@@ -81,3 +81,63 @@ namespace Data
 		numT z;
 	};
 }
+
+template<class numT>
+Data::Position<numT>::Position()
+{
+	this->x = numT();
+	this->y = numT();
+	this->z = numT();
+}
+
+template<class numT>
+Data::Position<numT>::Position(const numT& x, const numT& y, const numT& z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+template<class numT>
+const numT& Data::Position<numT>::getX() const
+{
+	return this->x;
+}
+
+template<class numT>
+const numT& Data::Position<numT>::getY() const
+{
+	return this->y;
+}
+
+template<class numT>
+const numT& Data::Position<numT>::getZ() const
+{
+	return this->z;
+}
+
+template<class numT>
+void Data::Position<numT>::setX(const numT& x)
+{
+	this->x = x;
+}
+
+template<class numT>
+void Data::Position<numT>::setY(const numT& y)
+{
+	this->y = y;
+}
+
+template<class numT>
+void Data::Position<numT>::setZ(const numT& z)
+{
+	this->z = z;
+}
+
+template<class numT>
+bool Data::Position<numT>::operator==(const Position<numT>& other) const
+{
+	return this->x == other.x
+		&& this->y == other.y
+		&& this->z == other.z;
+}
