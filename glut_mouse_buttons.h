eb70bb35857bd618d09d147d60c6c14898b1ee51
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2020-11-30 02:41:51 +0800

    Update Glut Mouse Buttons
    Added scroll up and scroll down options to the Display::FreeGlut::MouseButton enumeration.

--- glut_mouse_buttons.h
+++ glut_mouse_buttons.h
@@ -10,7 +10,9 @@ namespace Display
 		{
 			LEFT = GLUT_LEFT_BUTTON,
 			MIDDLE = GLUT_MIDDLE_BUTTON,
-			RIGHT = GLUT_RIGHT_BUTTON
+			RIGHT = GLUT_RIGHT_BUTTON,
+			SCROLL_UP = 3,
+			SCROLL_DOWN = 4
 		};
 	}
 }
