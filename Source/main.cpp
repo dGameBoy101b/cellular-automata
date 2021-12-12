3430cf0c9e43e0ba0b5a2e49afc261f94ebc3df4
dGameBoy101b <rhysmader@gmail.com>
2021-12-12 23:21:12 +0800

    Setup SDL2
    Setup SDL2 library with dynamic linking.
    Added a blank maximised main window.
    Added event processing which exits the application when the escape key is pressed.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,44 @@
+#define SDL_MAIN_HANDLED
+#include "SDL2/SDL.h"
+#include <iostream>
+
+/** Check if the given event should close the application
+\param event The event to process
+\return True if the application should now exit
+*/
+bool shouldExit(const SDL_Event& event)
+{
+	return event.type == SDL_QUIT
+		|| (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE);
+}
+
 int main(int argc, char** argv)
 {
-	return 0;
+	SDL_SetMainReady();
+	int error = SDL_InitSubSystem(SDL_INIT_VIDEO);
+	if (error != 0)
+	{
+		std::cout << SDL_GetError();
+		SDL_Quit();
+		return error;
+	}
+
+	auto main_window = SDL_CreateWindow("Cellular Automata", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
+	if (main_window == NULL)
+	{
+		std::cout << SDL_GetError();
+		SDL_Quit();
+		return -1;
+	}
+
+	SDL_Event event;
+	while (true)
+	{
+		if (SDL_PollEvent(&event) == 1 && shouldExit(event))
+		{
+			std::cout << "Exiting..." << std::endl;
+			SDL_Quit();
+			return 0;
+		}
+	}
 }
